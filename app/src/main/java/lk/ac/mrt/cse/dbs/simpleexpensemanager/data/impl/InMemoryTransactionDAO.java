7b0031aaab18b5c05c3cd5851684e0214fc74337
Gayashan Amarasinghe <gayashan@cse.mrt.ac.lk>
2015-12-02 00:32:16 +0530

    In-Memory transaction dao implementation added.

--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/impl/InMemoryTransactionDAO.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/impl/InMemoryTransactionDAO.java
@@ -1,9 +1,8 @@
 package lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl;
 
-import java.util.ArrayList;
 import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Stack;
 
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.TransactionDAO;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.ExpenseType;
@@ -13,7 +12,11 @@ import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Transaction;
  *
  */
 public class InMemoryTransactionDAO implements TransactionDAO {
-    private Stack<Transaction> transactions;
+    private List<Transaction> transactions;
+
+    public InMemoryTransactionDAO() {
+        transactions = new LinkedList<>();
+    }
 
     @Override
     public void logTransaction(Date date, String accountNo, ExpenseType expenseType, double amount) {
@@ -22,10 +25,18 @@ public class InMemoryTransactionDAO implements TransactionDAO {
     }
 
     @Override
-    public List<Transaction> getLogs() {
-        List<Transaction> transactionsList = new ArrayList<>();
-        transactionsList.addAll(transactions);
-        return transactionsList;
+    public List<Transaction> getAllTransactionLogs() {
+        return transactions;
+    }
+
+    @Override
+    public List<Transaction> getPaginatedTransactionLogs(int limit) {
+        int size = transactions.size();
+
+        if (size <= limit) {
+            return transactions;
+        }
+        return transactions.subList(size - limit, size);
     }
 
 }
