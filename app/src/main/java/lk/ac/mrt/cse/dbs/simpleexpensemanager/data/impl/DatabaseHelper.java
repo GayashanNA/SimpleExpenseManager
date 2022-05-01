2237ed71bb7f71351228c49dfce4685a02c97ee7
Jananga Kalawana <jananga1999@gmail.com>
2022-05-02 00:19:05 +0530

    invalid index number bug finished

--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/impl/DatabaseHelper.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/data/impl/DatabaseHelper.java
@@ -6,11 +6,11 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.exception.InvalidAccountException;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Account;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.ExpenseType;
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Transaction;
@@ -32,7 +32,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                 ")";
 
         String createTransactionTable = "create table transactionLog ("+
-                "transactionId integer primary key autoincrement, "+
                 "date text, " +
                 "accountNo text, " +
                 "expenseType text check(expenseType in ('EXPENSE','INCOME')), "+
@@ -56,7 +55,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public List<Account> getAccounts(){
         SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
         Cursor cursorAccounts = sqLiteDatabase.rawQuery("select * from account", null);
-        List<Account> accounts = new ArrayList<Account>();
+        List<Account> accounts = new ArrayList<>();
         if(cursorAccounts.moveToFirst()){
             do{
                 accounts.add(new Account(cursorAccounts.getString(0), cursorAccounts.getString(1), cursorAccounts.getString(2), cursorAccounts.getDouble(3)));
@@ -69,10 +68,18 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public List<Transaction> getTransactions(){
         SQLiteDatabase sqLiteDatabase = this.getReadableDatabase();
         Cursor cursorTransactions = sqLiteDatabase.rawQuery("select * from transactionLog", null);
-        List<Transaction> transactions = new ArrayList<Transaction>();
+        List<Transaction> transactions = new ArrayList<>();
+        transactions.add(new Transaction(new Date(), "asd", ExpenseType.INCOME, 23));
         if(cursorTransactions.moveToFirst()){
             do{
-                transactions.add(new Transaction(new Date(cursorTransactions.getString(1)), cursorTransactions.getString(2), ExpenseType.valueOf(cursorTransactions.getString(3)), cursorTransactions.getDouble(4)));
+
+                //transactions.add(new Transaction(new Date(), cursorTransactions.getString(2), ExpenseType.valueOf(cursorTransactions.getString(3)), 45));
+                try{
+                    transactions.add(new Transaction(new SimpleDateFormat("dd/MM/yyyy").parse(cursorTransactions.getString(1)), cursorTransactions.getString(2), ExpenseType.valueOf(cursorTransactions.getString(3)), cursorTransactions.getDouble(4)));
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+
             }while(cursorTransactions.moveToNext());
         }
         cursorTransactions.close();
@@ -94,11 +101,11 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public void addTransaction(Transaction transaction){
         SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put("date", transaction.getDate().toString());
+        contentValues.put("date", new SimpleDateFormat("dd/MM/yyyy").format(transaction.getDate()));
         contentValues.put("accountNo", transaction.getAccountNo());
         contentValues.put("expenseType", transaction.getExpenseType().name());
         contentValues.put("amount", transaction.getAmount());
-        sqLiteDatabase.insert("transaction", null, contentValues);
+        sqLiteDatabase.insert("transactionLog", null, contentValues);
         sqLiteDatabase.close();
     }
 
