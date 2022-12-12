b729461bdabed56dd41addc75459f8b47986f5f0
Sathveegan3105 <117098579+Sathveegan3105@users.noreply.github.com>
2022-12-13 04:41:24 +0530

    Add files via upload

---
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistentExpenseManager.java
@@ -0,0 +1,28 @@
+package lk.ac.mrt.cse.dbs.simpleexpensemanager.control;
+
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.AccountDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.TransactionDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.PersistentAccountDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.PersistentTransactionDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Account;
+
+public class PersistentExpenseManager extends ExpenseManager {
+    public PersistentExpenseManager() {
+        setup();
+    }
+
+    @Override
+    public void setup() {
+        AccountDAO sqliteMemoryAccountDAO = new PersistentAccountDAO();
+        setAccountsDAO(sqliteMemoryAccountDAO);
+
+        TransactionDAO sqliteTransactionDAO = new PersistentTransactionDAO();
+        setTransactionsDAO(sqliteTransactionDAO);
+
+        // dummy data
+        Account dummyAcct1 = new Account("12345eA", "Yoda Bank", "Anakin Skywalker", 10000.0);
+        Account dummyAcct2 = new Account("78945Z", "Clone BC", "Obi-Wan Kenobi", 80000.0);
+        getAccountsDAO().addAccount(dummyAcct1);
+        getAccountsDAO().addAccount(dummyAcct2);
+    }
+}
